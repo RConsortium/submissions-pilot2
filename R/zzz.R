c1189c83f2fc7fbf6ccc541c68c940f7a2f7cbb3
Eric Nantz <theRcast@gmail.com>
2022-07-14 20:23:36 +0000

    disable startup function due to errors

--- R/zzz.R
+++ R/zzz.R
@@ -2,13 +2,14 @@
 # The file is to load startup file automatically when the package is loaded.
 
 .onLoad <- function(libname, pkgname) {
-  startup <- system.file("startup.R", package = pkgname)
-
-  if (file.exists(startup)) {
-    source(startup)
-  } else {
-    stop("Can not find", startup)
-  }
-
-  invisible(startup)
+  message("Submissions Pilot 2 Shiny App")
+  # startup <- system.file("startup.R", package = pkgname)
+  # 
+  # if (file.exists(startup)) {
+  #   source(startup)
+  # } else {
+  #   stop("Can not find", startup)
+  # }
+  # 
+  # invisible(startup)
 }
